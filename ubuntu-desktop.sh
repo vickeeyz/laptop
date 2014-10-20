0907936c55fcc651932eab5b4efe825727b7479f
Zhiyuan Shi <zhiyuanshi@users.noreply.github.com>
2014-10-20 16:44:12 +0800

    Add VirtualBox.

--- ubuntu-desktop.sh
+++ ubuntu-desktop.sh
@@ -10,3 +10,14 @@ curl -L https://raw.githubusercontent.com/chriskempson/base16-gnome-terminal/mas
 # SmartGit/Hg
 
 # Sublime Text 3
+
+# VirtualBox
+#
+# Ubuntu/Debian users might want to install the dkms package to ensure that the
+# VirtualBox host kernel modules (vboxdrv, vboxnetflt and vboxnetadp) are
+# properly updated if the linux kernel version changes during the next apt-get
+# upgrade.
+#
+# https://www.virtualbox.org/wiki/Linux_Downloads
+sudo apt-get -y install virtualbox
+sudo apt-get -y install dkms
