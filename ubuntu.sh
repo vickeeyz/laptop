3f25d6c2b932f8feb1e50228892ce177d08d442c
Zhiyuan Shi <zhiyuanshi@users.noreply.github.com>
2014-11-06 06:36:53 +0800

    texlive is the culprit

--- ubuntu.sh
+++ ubuntu.sh
@@ -180,4 +180,4 @@ curl -s https://static.rust-lang.org/rustup.sh | sudo sh
 # sudo apt-get -y install texlive-math-extra   # contains stmaryrd.sty
 
 # Have to use this. Otherwise mathastext.sty can still not be found.
-sudo apt-get -y install texlive-full
+# sudo apt-get -y install texlive-full
