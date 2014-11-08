f85fa9a1dd9d4087b50fe053f32f995005cc5394
Zhiyuan Shi <zhiyuanshi@users.noreply.github.com>
2014-11-08 13:03:39 +0800

    Add Erlang

--- ubuntu.sh
+++ ubuntu.sh
@@ -175,6 +175,9 @@ sudo dpkg -i /tmp/sbt-${SBT_VERSION}.deb
 sudo apt-get update
 sudo apt-get -y install sbt
 
+# Erlang
+sudo apt-get -y install erlang
+
 # Rust
 # http://doc.rust-lang.org/guide.html
 curl -s https://static.rust-lang.org/rustup.sh | sudo sh
