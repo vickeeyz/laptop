6e0fef095a658568a049718cef9fd0c8002813e1
Zhiyuan Shi <zhiyuanshi@users.noreply.github.com>
2014-11-09 11:56:23 +0800

    Revert "Add IntelliJ IDEA"
    This reverts commit fefa1c0e88455327f911ea900a6697bdd9088045.

--- ubuntu-desktop.sh
+++ ubuntu-desktop.sh
@@ -24,20 +24,6 @@ sudo apt-get -y install vim-gnome
 # "Open in Terminal"
 sudo apt-get -y install nautilus-open-terminal
 
-# IntelliJ IDEA
-intellij_tar_file=ideaiu-14.tar.gz
-intellij_dir_name=idea-IU-139.224.1
-
-cd /tmp
-curl -O http://download-cf.jetbrains.com/idea/$intellij_tar_file
-tar xfz $intellij_tar_file
-
-sudo mkdir -p /usr/local/intellj
-sudo mv $intellj_dir_name /usr/local/intellj
-
-sudo ln -s --force /usr/local/intellj/$intellj_dir_name/bin/idea.sh /usr/local/bin/idea
-cd "$whereiwas"
-
 # VirtualBox
 #
 # Ubuntu/Debian users might want to install the dkms package to ensure that the
